abcbdd7571b2ea18ad51ce1ecbb1247da70bd80c
Rocío Martín <149075907+Rocio-fullstack@users.noreply.github.com>
2024-04-26 13:56:29 +0000

    First commit

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -4,13 +4,15 @@ const TrafficLight = () => {
     const [selectedLight, setSelectedLight] = useState("");
 
     return (
-        <div className="semaforo">
+        
+         <div className="semaforo">
             <div onClick={()=> {setSelectedLight("red")}} className={`red ${selectedLight == "red" ? "shine":""}`}></div>
             <div onClick={()=> {setSelectedLight("yellow")}}className={`yellow ${selectedLight == "yellow" ? "shine":""}`}></div>
             <div onClick={()=> {setSelectedLight("green")}} className={`green ${selectedLight == "green" ? "shine":""}`}></div>
             <div onClick={()=> {setSelectedLight("black")}} className={`black ${selectedLight == "black" ? "shine":""}`}></div>
 
         </div>
+        
 
     )
 }
